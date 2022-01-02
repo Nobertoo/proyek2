124b9a91ddc524d593884713eaf1922bb4f44b75
StartUpTech21 <78974051+StartUpTech21@users.noreply.github.com>
2022-01-03 03:00:15 +0700

    algoritman fix

--- system/app/Http/Controllers/ClientController.php
+++ system/app/Http/Controllers/ClientController.php
@@ -1,6 +1,7 @@
 <?php 
 namespace App\Http\Controllers; 
 use App\Models\Penyakit;
+use DB;
 
 class ClientController extends Controller{
 	function Beranda(){
@@ -8,8 +9,43 @@ class ClientController extends Controller{
 
 	}
 
-	function Penyakit(){
-		$data['list_penyakit'] = Penyakit::all();
-		return view('penyakitclient', $data); 
+	function diagnosaGejala(){
+		// $data['list_penyakit'] = Penyakit::all();
+		// return view('penyakitclient', $data); 
+		$data['list_gejala'] = DB::table('gejala')->select('*')->get();
+		// dd($data['list_gejala']);
+		return view('index', $data); 
+	}
+
+	function data1(){
+		$request_gejala = request()->kode_gejala;
+		$hasil = $request_gejala;
+		// dd($hasil);
+
+		if($request_gejala == ["G001","G002","G003","G004","G005","G009"]){
+			// Query_diagnosa
+			$data['list_penyakit'] = DB::table('penyakit')
+				->select("penyakit.kode_penyakit", "penyakit.nama_penyakit", DB::raw("penyakit.keterangan as p_ket"), "solusi.kode_solusi", DB::raw("solusi.keterangan as s_ket"))
+				->join('solusi','penyakit.id','solusi.id_penyakit')
+				->where("kode_penyakit","P001")
+				->get();
+				// dd($data['list_penyakit']);
+		}elseif($request_gejala == ["G001","G002","G003","G004","G005","G006","G007","G010","G013","G015"]){
+			$data['list_penyakit'] = DB::table('penyakit')
+				->select("penyakit.kode_penyakit", "penyakit.nama_penyakit", DB::raw("penyakit.keterangan as p_ket"), "solusi.kode_solusi", DB::raw("solusi.keterangan as s_ket"))
+				->join('solusi','penyakit.id','solusi.id_penyakit')
+				->where("kode_penyakit","P002")
+				->get();
+				dd($data['list_penyakit']);
+		}else{
+			$data['list_penyakit'] = "tidak ada";
+		}
+		return view('data1',$data);
 	}
 }
+
+// Query_diagnosa
+// SELECT penyakit.kode_penyakit, penyakit.nama_penyakit, penyakit.keterangan as p_ket, solusi.kode_solusi, solusi.keterangan as s_ket
+// FROM `penyakit` 
+// join solusi on penyakit.id=solusi.id_penyakit
+// where kode_penyakit='P001';
---
+++ system/resources/views/data1.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Penyakit</title>
+</head>
+<body>
+
+<table border="1">
+	<tr>
+		<th>kode_penyakit</th>
+		<th>nama_penyakit</th>
+		<th>keterangan_penyakit</th>
+		<th>kode_solusi</th>
+		<th>solusi_keterangan</th>
+	</tr>
+	@foreach($list_penyakit as $d)
+	<tr>
+		<td>{{$d->kode_penyakit}}</td>
+		<td>{{$d->nama_penyakit}}</td>
+		<td>{{$d->p_ket}}</td>
+		<td>{{$d->kode_solusi}}</td>
+		<td>{{$d->s_ket}}</td>
+	</tr>
+	@endforeach
+</table>
+
+</body>
+</html>
+
+
